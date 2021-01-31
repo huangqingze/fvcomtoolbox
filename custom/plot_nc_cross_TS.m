aeade0b9462fb8d8a870c1fe5852ee361e1328bd
dlxiii <tokyowangyulong@gmail.com>
2021-01-31 22:06:07 +0900

    Update plot_nc_cross_TS.m

--- custom/plot_nc_cross_TS.m
+++ custom/plot_nc_cross_TS.m
@@ -26,12 +26,24 @@ for i =1:length(vartoplot)
     fig = figure(Num+i);
     %clf
     fprintf([vartoplot{i},': cross section profile.','\n']);
-
-    minval = min(min(min(ncfile.(vartoplot{i})(crs_node,:,:))));
-    maxval = max(max(max(ncfile.(vartoplot{i})(crs_node,:,:))));
-    for tt =1:(timestep-1):length(ncfile.Times)
-        sudata = reshape(ncfile.(vartoplot{i})(crs_node,:,tt),[size(crs_node,1)*fz,1]);
-        DateString = ncfile.Times(tt,:);
+    try
+        minval = min(min(min(ncfile.(vartoplot{i})(crs_node,:,:))));
+        maxval = max(max(max(ncfile.(vartoplot{i})(crs_node,:,:))));
+    catch
+        minval = min(min(ncfile.(vartoplot{i})(crs_node,:)));
+        maxval = max(max(ncfile.(vartoplot{i})(crs_node,:)));
+    end
+    for tt =1:(timestep-1):length(ncfile.Times(:,1))
+        try
+            sudata = reshape(ncfile.(vartoplot{i})(crs_node,:,tt),[size(crs_node,1)*fz,1]);
+        catch
+            sudata = reshape(ncfile.(vartoplot{i})(crs_node,:,1),[size(crs_node,1)*fz,1]);
+        end
+        try
+            DateString = ncfile.Times(tt,:);
+        catch
+            DateString = ncfile.Times(1,:);
+        end
         plot_cross(Num+i, depsig, lonsig, sudata, (vartoplot{i}),'cross section',...
             minval,...
             maxval,...
@@ -43,7 +55,7 @@ for i =1:length(vartoplot)
     end
     % Save anamation files
     filename = ['plot_NC_cross_',vartoplot{i},'_',num2str(length(crs_node)),'.gif']; % Specify the output file name
-    for tt =1:(timestep-1):length(ncfile.Times)
+    for tt =1:(timestep-1):length(ncfile.Times(:,1))
         [A,map] = rgb2ind(im{tt},256);
         if tt == 1
             imwrite(A,map,filename,'gif','LoopCount',Inf,'DelayTime',1);
