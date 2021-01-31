2a8e92bceba8ffc45209814b78eff0722fb86ec0
dlxiii <tokyowangyulong@gmail.com>
2021-01-31 22:06:09 +0900

    Update plot_nc_layer_TS.m

--- custom/plot_nc_layer_TS.m
+++ custom/plot_nc_layer_TS.m
@@ -21,12 +21,20 @@ for k = 1:length(vartoplot)
         fprintf([vartoplot{k},': ',num2str(layer),' of layer ',num2str(length(ncfile.siglay(1,:))),'\n']);
         minval = min(min(min(ncfile.(vartoplot{k})(:,layer,:))));
         maxval = max(max(max(ncfile.(vartoplot{k})(:,layer,:))));
-        for j = 1:(timestep-1):length(ncfile.Times)
+        for j = 1:(timestep-1):length(ncfile.Times(:,1))
+            try
             plot_VALUE(Num+k, [ncfile.lon, ncfile.lat], ncfile.nv, ncfile.(vartoplot{k})(:,layer,j),...
                 vartoplot{k}, ['layer No.',num2str(layer,'%02.2i')],...
                 minval,...
                 maxval,...
                 [ncfile.Times(j,1:10),' ',ncfile.Times(j,12:19)]);
+            catch
+            plot_VALUE(Num+k, [ncfile.lon, ncfile.lat], ncfile.nv, ncfile.(vartoplot{k})(:,layer,j),...
+                vartoplot{k}, ['layer No.',num2str(layer,'%02.2i')],...
+                minval,...
+                maxval,...
+                [ncfile.Times(j,1:7)]);
+            end
             drawnow
             frame = getframe(fig);
             im{j} = frame2im(frame);
@@ -34,7 +42,7 @@ for k = 1:length(vartoplot)
         end
         % close;
         filename = ['plot_NC_layer_',num2str(layer,'%02.2i'),'_',vartoplot{k},'.gif']; % Specify the output file name
-        for j = 1:(timestep-1):length(ncfile.Times)
+        for j = 1:(timestep-1):length(ncfile.Times(:,1))
             [A,map] = rgb2ind(im{j},256);
             if j == 1
                 imwrite(A,map,filename,'gif','LoopCount',Inf,'DelayTime',1);
